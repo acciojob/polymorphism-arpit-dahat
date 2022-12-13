0a3d6319cdf760152e9a952c371cc79a4a27be01
Arpit <arpitdahat.ad@gmail.com>
2022-12-13 09:57:18 +0530

    Code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,27 @@
 package com.driver;
 
 public class Main {
+   public static class Product{
+        public int product(int x, int y){
+            return x*y;
+        }
+       public int product(int x, int y, int z){
+            return x*y*z;
+       }
+       public double product(double x, double y){
+            return x*y;
+
+       }
+
+    }
+
+    public static void main(String[] args) {
+        Product p = new Product();
+        int ans = p.product(2,4);
+        int ans1 = p.product(2,5,7);
+        double ans2 = p.product(2.4,2.5);
+        System.out.println(ans + " " + ans1 + " " + ans2);
+
+    }
 
 }
